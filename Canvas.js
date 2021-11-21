c629b534f2190ab5384ffc17d64427c3676d096a
gapizaver <gapi.levacic@gmail.com>
2021-11-21 15:46:51 +0100

    +on the spot rendering when draging new curve +ability to drag points

--- Canvas.js
+++ Canvas.js
@@ -5,10 +5,13 @@ import { Vector } from "./Vector.js";
 export class Canvas {
     constructor(canvas) {
         this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
         this.splines = [];
         this.currentSpline = 0;
         this.currentCurve = 0;
         this.pointSize = 3;
+        this.selectedPoints = [];
+        this.selectedControlPoint = false;
 
         // listeners
         canvas.addEventListener("mousemove", (e) => {
@@ -33,60 +36,157 @@ export class Canvas {
         let x = e.offsetX;
         let y = e.offsetY;
 
-        if (this.currentCurve == 0 ) {
-            if (typeof this.splines[this.currentSpline] == "undefined") {    // first mouse down of the spline
-                this.splines.push(new Spline([
-                    new Bezier([new Vector([x,y]), null, null, null])
-                ]));
-            } else {                                                         // second mouse down of the spline
-                this.splines[this.currentSpline].curves[0].points[2] = new Vector([x, y]);
+        // check if mouse pos on existing Point
+        this.selectedPoints = [];
+
+        for (let i = 0; i < this.splines.length; i++) {
+            for (let j = 0; j < this.splines[i].curves.length; j++) {
+                for (let k = 0; k < 4; k++) {
+                    // check if mouse pos inside Point
+                    let curve = this.splines[i].curves[j]
+                    let point = curve.points[k];
+
+                    // if point exists
+                    if (point != null) {            
+                        // if mouse cursor inside point
+                        if (Math.abs(point.coor[0] - x) < this.pointSize &&
+                                    Math.abs(point.coor[1] - y) < this.pointSize) {
+                            // add selected point
+                            this.selectedPoints.push(point);
+
+                            // select as interdependent points
+                            switch (k) {
+                                case 0:
+                                    // add control point
+                                    this.selectedPoints.push(curve.points[1]);
+                                    // add main point
+                                    this.selectedPoints.push(curve.points[0]);
+                                    // add previous curve's (if it exists) control point
+                                    if (j > 0) {
+                                        this.selectedPoints.push(this.splines[i].curves[j-1].points[3]);
+                                    }
+                                    break;
+                                case 1:
+                                    // add main point and previous related control point (if previous curve exitst)
+                                    if (j > 0) {
+                                        this.selectedPoints.push(this.splines[i].curves[j-1].points[3]);
+                                        this.selectedPoints.push(this.splines[i].curves[j-1].points[2]);
+                                    }
+                                    // flag that this is control point
+                                    this.selectedControlPoint = true;
+                                    break;
+                                case 2:
+                                    // add main point and next related control point (if next curve exists)
+                                    if (j < this.splines[i].curves.length - 1) {
+                                        this.selectedPoints.push(this.splines[i].curves[j+1].points[0]);
+                                        this.selectedPoints.push(this.splines[i].curves[j+1].points[1]);
+                                    }
+                                    // flag that this is control point
+                                    this.selectedControlPoint = true;
+                                    break;
+                                case 3:
+                                    // add control point
+                                    this.selectedPoints.push(curve.points[2])
+                                    // add next curve's (if it exists) related control point
+                                    if (j < this.splines[i].curves.length - 1) {
+                                        this.selectedPoints.push(this.splines[i].curves[j+1].points[1]);
+                                    }
+                                    break;
+                            }
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        // if no points selected add new curve
+        if (this.selectedPoints.length == 0) {
+            if (this.currentCurve == 0 ) {
+                if (typeof this.splines[this.currentSpline] == "undefined") {    
+                    // first mouse down of the spline
+                    let point_to_move = new Vector([x,y]);
+                    this.splines.push(new Spline([
+                        new Bezier([new Vector([x,y]), point_to_move, null, null])
+                    ]));
+                    this.selectedPoints = [point_to_move];
+                } else {
+                    // second mouse down of the spline
+                    let point_to_move = new Vector([x, y]);
+                    this.splines[this.currentSpline].curves[0].points[2] = new Vector([x, y]);
+                    this.splines[this.currentSpline].curves[0].points[3] = point_to_move;
+                    this.selectedPoints = [point_to_move];
+                    this.currentCurve++;
+                }
+            } else {
+                // set coordinates of the first point of new bezier to the coordinates of the
+                // last point of last bezier
+                let b = new Bezier([null, null, null, null]);
+                let x3_last = this.splines[this.currentSpline].curves[this.currentCurve-1].points[3];
+                b.points[0] = x3_last;
+                
+                // calculate coordinates of the second control point
+                let x2_last = this.splines[this.currentSpline].curves[this.currentCurve-1].points[2];
+                let x1_new = x2_last.sub(x3_last).mulScalar(-1).add(x3_last);
+                b.points[1] = x1_new;
+
+                // set third point to the location of mouseDown 
+                b.points[2] = new Vector([x, y]);
+
+                // set fourth point to selected point
+                let point_to_move = new Vector([x, y]);
+                b.points[3] = point_to_move;
+                this.selectedPoints = [point_to_move];
+
+                // add new curve to the spline
+                this.splines[this.currentSpline].curves.push(b);
                 this.currentCurve++;
             }
-        } else {
-            // set coordinates of the first point of new bezier to the coordinates of the
-            // last point of last bezier
-            let b = new Bezier([null, null, null, null]);
-            let x3_last = this.splines[this.currentSpline].curves[this.currentCurve-1].points[3];
-            b.points[0] = x3_last;
-            
-            // calculate coordinates of the second point
-            let x2_last = this.splines[this.currentSpline].curves[this.currentCurve-1].points[2];
-            let x1_new = x2_last.sub(x3_last).mulScalar(-1).add(x3_last);
-            b.points[1] = x1_new;
-
-            // set third point to the location of mouseDown 
-            b.points[2] = new Vector([x, y]);
-
-            // add new curve to the spline
-            this.splines[this.currentSpline].curves.push(b);
-            this.currentCurve++;
         }
     }
 
     onMouseUp(e) {
-        if (typeof this.splines[this.currentSpline] != "undefined") {
-            let x = e.offsetX;
-            let y = e.offsetY;
+        // deselect selected points
+        this.selectedPoints = []
+        this.selectedControlPoint = false;
 
-            // first mouse up
-            if (this.currentCurve == 0 ) {
-                if (this.splines[this.currentSpline].curves[this.currentCurve].points[1] == null) {     // first mouse down of the spline
-                    this.splines[this.currentSpline].curves[this.currentCurve].points[1] = new Vector([x, y]);
-                }
-            } else if (this.currentCurve == 1) {       // second mouse up of the spline
-                    this.splines[this.currentSpline].curves[this.currentCurve-1].points[3] = new Vector([x, y]);
-            } else {
-                // set coordinates of fourth point to the location of mouseUp
-                this.splines[this.currentSpline].curves[this.currentCurve-1].points[3] = new Vector([x, y]);
-            }
-            
-            this.render();
-        } else {
-            console.log("onMouseUp this.splines[this.currentspline] not defined");
-        }
+        // set cursor to default
+        this.canvas.style.cursor = "default";
     }
 
     onMouseMove(e) {
+        let x = e.offsetX;
+        let x_movement = e.movementX;
+        let y = e.offsetY;
+        let y_movement = e.movementY;
+
+        if (this.selectedPoints.length > 0) {
+            // change cursor to "move"
+            this.canvas.style.cursor = "move";
+        } else {
+            this.canvas.style.cursor = "default";
+        }
+        
+        // move selected points
+        if (this.selectedControlPoint && this.selectedPoints.length > 1) {
+            // control point selected, other control point's coordinates have to be calculated
+            let cp1 = this.selectedPoints[0];   // selected control point
+            let mp = this.selectedPoints[1];    // main point
+            let cp2 = this.selectedPoints[2];   // related control point
+
+            cp1.coor = [x, y];
+            // calculate related control point's coordinates
+            cp2.coor = cp1.sub(mp).mulScalar(-1).add(mp).coor;
+        } else {
+            // move selected points
+            for (let i = 0; i < this.selectedPoints.length; i++) {
+                this.selectedPoints[i].coor =
+                        this.selectedPoints[i].add(new Vector([x_movement, y_movement])).coor;
+            }
+        }
+
+        // render
+        this.render();
     }
 
     newSpline() {
@@ -100,9 +200,13 @@ export class Canvas {
     }
 
     render() {
+        // clear screen
+        let canvas = document.getElementById("canvas");
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         for (let i = 0; i < this.splines.length; i++) {
-            //draw line
-            if (this.currentCurve > 0) {
+            //draw curve
+            if (this.splines[i].curves.length > 0 && this.splines[i].curves[0].points[2] != null) {
                 this.ctx.setLineDash([]);
 
                 this.ctx.beginPath();
